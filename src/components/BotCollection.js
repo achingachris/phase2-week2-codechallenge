cbc90ac18bd292e1d32c241fd98b0d83118bb77b
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-04 01:55:27 +0300

    implement DRY

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import BotCard from './BotCard'
 
-function BotCollection({ botCollection, action, removeCard }) {
-  const displayBotCards = botCollection.map((bot) => {
-    return <BotCard bot={bot} action={action} removeCard={removeCard} />
-  })
-
+const BotCollection = ({ botCollection, action, removeCard }) => {
   return (
     <div className='ui four column grid'>
-      <div className='row'>{displayBotCards}</div>
+      <div className='row'>
+        {botCollection.map((bot) => (
+          <BotCard bot={bot} action={action} removeCard={removeCard} />
+        ))}
+      </div>
     </div>
   )
 }
