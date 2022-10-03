003aea32551f2f3419299d3c354c5dfb729c40e2
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-03 22:17:19 +0300

    use usefeects

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function BotCollection() {
-  // Your code here
+  const [bots, setBots] = useState([])
+  const API_URL = 'http://localhost:8002/bots'
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <div className='ui four column grid'>
-      <div className='row'>
-        {/*...and here..*/}
-        Collection of all bots
-      </div>
+      <div className='row'></div>
     </div>
   )
 }
