0aa5763c92d2779ddd22fbe60c4ff6c417375b5d
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-04 01:53:42 +0300

    restructure

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,24 +1,12 @@
 import React from 'react'
 
-const botTypeClasses = {
-  Assault: 'icon military',
-  Defender: 'icon shield',
-  Support: 'icon plus circle',
-  Medic: 'icon ambulance',
-  Witch: 'icon magic',
-  Captain: 'icon star',
-}
-
-const BotCard = (props) => {
-  const { bot, action, removeCard } = props
-
-  function handleClick(e) {
+const BotCard = ({ bot, action, removeCard }) => {
+  const handleClick = (e) => {
     console.log('handleClick fired!')
-    // e.stopPropagation()
     action(bot)
   }
 
-  function handleDischarge(e) {
+  const handleDischarge = (e) => {
     console.log('Red X Clicked!')
     e.stopPropagation()
     removeCard(bot)
@@ -26,32 +14,32 @@ const BotCard = (props) => {
 
   return (
     <div className='ui column'>
-      <div className='ui card' key={props.bot.id} onClick={handleClick}>
+      <div className='ui card' key={bot.id} onClick={handleClick}>
         <div className='image'>
-          <img alt='oh no!' src={props.bot.avatar_url} />
+          <img alt='oh no!' src={bot.avatar_url} />
         </div>
         <div className='content'>
           <div className='header'>
-            {props.bot.name}
-            <i className={botTypeClasses[props.bot.bot_class]} />
+            {bot.name}
+            <i className={botTypeClasses[bot.bot_class]} />
           </div>
           <div className='meta text-wrap'>
-            <small>{props.bot.catchphrase}</small>
+            <small>{bot.catchphrase}</small>
           </div>
         </div>
         <div className='extra content'>
           <span>
             <i className='icon heartbeat' />
-            {props.bot.health}
+            {bot.health}
           </span>
 
           <span>
             <i className='icon lightning' />
-            {props.bot.damage}
+            {bot.damage}
           </span>
           <span>
             <i className='icon shield' />
-            {props.bot.armor}
+            {bot.armor}
           </span>
           <span>
             <div className='ui center aligned segment basic'>
@@ -67,3 +55,13 @@ const BotCard = (props) => {
 }
 
 export default BotCard
+
+// styles
+const botTypeClasses = {
+  Assault: 'icon military',
+  Defender: 'icon shield',
+  Support: 'icon plus circle',
+  Medic: 'icon ambulance',
+  Witch: 'icon magic',
+  Captain: 'icon star',
+}
