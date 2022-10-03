43a634dd0b43b6bbbe9a30c35082cb02af614914
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-04 01:46:55 +0300

    list-bots

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import YourBotArmy from './YourBotArmy'
 import BotCollection from './BotCollection'
 
 function BotsPage() {
-  //start here with your code for step one
+  const API_URL = 'http://localhost:8002/bots'
+
+  const [selectedBots, setSelectedBots] = useState([])
+  const [bots, setBots] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setBots(data))
+  }, [])
+
+  // get the bot that was clicked on
+  // add it to the selectedBots array
+  function handleBotClick(bot) {
+    setSelectedBots([...selectedBots, bot])
+  }
 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy displayedBots={handleBotClick} />
+      <BotCollection data={bots} />
     </div>
   )
 }
