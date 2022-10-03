7aade69502e2de9e307ed0ee100fa19e43f710fe
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-04 01:26:20 +0300

    bot collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import BotCard from './BotCard'
 
-function BotCollection() {
-  const [bots, setBots] = useState([])
-  const API_URL = 'http://localhost:8002/bots'
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setBots(data))
-  }, [])
+function BotCollection({ botCollection, action, removeCard }) {
+  const displayBotCards = botCollection.map((bot) => {
+    return <BotCard bot={bot} action={action} removeCard={removeCard} />
+  })
 
   return (
     <div className='ui four column grid'>
-      <div className='row'>
-        {bots.map((data) => (
-          <BotCard key={data.id} bot={data} />
-        ))}
-      </div>
+      <div className='row'>{displayBotCards}</div>
     </div>
   )
 }
